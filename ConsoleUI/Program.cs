08291666cd3afbed30f33b571c7b6861ae499212
ezgi_aydin_28@hotmail.com <ezgi_aydin_28@hotmail.com>
2021-02-14 00:37:39 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductTest();
+            //CategoryTest();
+            ProductDetailTest();
+        }
+
+        private static void ProductDetailTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var productDetail in productManager.GetProductDetails())
+            {
+                Console.WriteLine("product id:  " + productDetail.ProductId + "/  "+
+                                  "product name:  " + productDetail.ProductName + "/  " +
+                                  "category name:  " + productDetail.CategoryName + "/  " +
+                                  "units in stock:  " + productDetail.UnitsInStock);
+            }
+
+        }
+
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+
+    }
+}
